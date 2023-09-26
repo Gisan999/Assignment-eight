efc790dca64d76a6999ca1fea446387319f77d68
Gisan999 <ruheatheking3@gmail.com>
2023-09-27 02:50:34 +0600

    I have solved some problems

--- src/Components/CardDetails/CardDetails.jsx
+++ src/Components/CardDetails/CardDetails.jsx
@@ -24,19 +24,24 @@ const CardDetails = () => {
     }
     return (
         <div className="max-w-screen-xl mx-auto mt-11">
-            <div className=" flex justify-center h-full lg:w-[1280px]  relative rounded-xl">
-                <img src={picture} className="w-96 md:w-3/4 lg:w-full h-full rounded-xl " />
+            <div className=" flex justify-center relative rounded-xl">
+                <img src={picture} className="w-full h-full rounded-xl px-8 lg:px-0" />
+
             </div>
-            <div className=" bg-black py-9 w-96 md:w-3/4 lg:w-full h-14 md:h-28 rounded-b-xl relative bottom-16 md:bottom-28 mix-blend-overlay left-[21px] md:left-24 lg:left-0">
+
+            <div className="px-8 lg:px-0">
+                <div className=" h-16 md:h-28 w-full bg-black rounded-b-xl relative bottom-16 md:bottom-28 mix-blend-overlay ">
+                </div>
             </div>
+
             <button onClick={handleLoadData}
                 style={{ 'background': text_color }}
-                className="relative bottom-[120px] md:bottom-48 left-12 md:left-28 lg:left-10  middle none center rounded-lg py-3 px-6 font-sans text-sm md:text-md font-bold uppercase text-white shadow-md  transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                className="relative bottom-[120px] md:bottom-48 left-12 md:left-28 lg:left-10  middle none center rounded-lg py-2 md:py-3 px-4 md:px-6 font-sans text-sm md:text-md font-bold uppercase text-white shadow-md  transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 data-ripple-light="true"
             >
                 Donate {price}
             </button>
-            <div className="relative bottom-20 space-y-5 px-5 lg:px-0">
+            <div className="relative bottom-20 space-y-5 px-8 lg:px-0">
                 <h2 className="text-4xl font-bold">{title}</h2>
                 <p>{description}</p>
             </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -41,14 +41,14 @@ const Home = () => {
                     <div className="absolute  inset-0 m-0 overflow-hidden rounded-none bg-transparent bg-[url('https://i.ibb.co/GFYQ0gt/cover.jpg')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
                         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-white/90 via-white/90 to-white/90"></div>
                     </div>
-                    <div className="relative p-6 py-48 px-6 md:px-12">
-                        <h2 className="mb-6 pb-8 block font-sans text-5xl font-bold leading-[1.5] tracking-normal text-black antialiased">
+                    <div className="relative p-6 py-20 md:py-40 lg:py-60 px-6 md:px-12">
+                        <h2 className="mb-6 pb-8 block font-sans  text-3xl md:text-5xl font-bold leading-[1.5] tracking-normal text-black antialiased">
                             I Grow By Helping People In Need
                         </h2>
                         <div className="flex justify-center items-center">
                             <form
                                 onSubmit={handleFieldBtn}>
-                                <input className="py-[6px] pl-2 pr-20 md:pr-48 bg-transparent border-2 border-gray-300 rounded-md" type="text" placeholder="Search here...." name="name" />
+                                <input className="py-[6px] pl-2 pr-12 md:pr-48 bg-transparent border-2 border-gray-300 rounded-md" type="text" placeholder="Search here...." name="name" />
                                 <input className="bg-[#FF444A] text-white py-2 px-4 relative right-4 rounded-r-lg" type="submit" value="Search" />
                             </form>
                         </div>
